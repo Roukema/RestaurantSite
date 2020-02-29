1cc74bda11cf277e62db78629f0533f16a75f95a
Roukema <58312286+Roukema@users.noreply.github.com>
2020-02-29 23:24:12 +0100

    start with review slide, marker google map still doenst work

--- src/Components/Contact/Map/Map.js
+++ src/Components/Contact/Map/Map.js
@@ -12,16 +12,11 @@ function AdressOnMap() {
         zoom={13}
         initialCenter={{ lat: 51.9228, lng: 4.47604 }}
       />
-      <Marker
-        id="marker"
-        position={{ lat: 51.9228, lng: 4.47604 }}
-        text={"markert"}
-        icon={"../Header/logo-5.svg"}
-      />
+      <Marker id="marker" position={{ lat: 51.9228, lng: 4.47604 }} />
     </div>
   );
 }
-console.log(API_key);
+
 export default GoogleApiWrapper({
   apiKey: API_key
 })(AdressOnMap);
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./Reviews.css";
+import RatingStars from "./Rating/Rating";
 
 function Reviews() {
   return (
     <div className="reviews">
-      <p> reviews</p>
+      <div className="review">
+        <img></img>
+        <h4>Name</h4>
+        {/* <RatingStars rating="2" /> */}
+        <p>★★★★★</p>
+        <p> Goed verhaal leuke winkel</p>
+      </div>
+      <div className="review">
+        <img></img>
+        <h4>Name2</h4>
+        {/* <RatingStars rating="2" /> */}
+        <p>★★★★</p>
+        <p> Goed verhaal aardige winkel</p>
+      </div>
+      <div className="review">
+        <img></img>
+        <h4>Name3</h4>
+        {/* <RatingStars rating="2" /> */}
+        <p>★★</p>
+        <p> Goed verhaal matig winkel</p>
+      </div>
     </div>
   );
 }
